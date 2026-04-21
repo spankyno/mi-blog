ceb91c3d0e40611176f2cece829bc5fc7f782d9f
Kalbo <spankyelmalo@yahoo.es>
2026-04-21 08:23:23 +0000

    fix: usar auth_type implicit en Decap CMS

--- oauth-worker/index.js
+++ oauth-worker/index.js
@@ -7,31 +7,61 @@ export default {
         client_id: env.GITHUB_CLIENT_ID,
         redirect_uri: env.REDIRECT_URI,
         scope: 'repo,user',
-        state: crypto.randomUUID(),
       });
       return Response.redirect(`https://github.com/login/oauth/authorize?${params}`, 302);
     }
 
     if (url.pathname === '/callback') {
       const code = url.searchParams.get('code');
+      
       const tokenRes = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+        headers: { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
         body: JSON.stringify({
           client_id: env.GITHUB_CLIENT_ID,
           client_secret: env.GITHUB_CLIENT_SECRET,
           code,
         }),
       });
-      const { access_token } = await tokenRes.json();
-      const script = `
-        <script>
-          window.opener.postMessage(
-            'authorization:github:success:${JSON.stringify({ token: access_token, provider: 'github' })}',
-            '*'
-          );
-        </script>`;
-      return new Response(script, { headers: { 'Content-Type': 'text/html' } });
+      
+      const data = await tokenRes.json();
+      const token = data.access_token;
+      
+      const html = `<!DOCTYPE html>
+<html>
+<body>
+<script>
+  (function() {
+    function receiveMessage(e) {
+      console.log("receiveMessage %o", e);
+    }
+    window.addEventListener("message", receiveMessage, false);
+    
+    const message = JSON.stringify({
+      token: "${token}",
+      provider: "github"
+    });
+    
+    if (window.opener) {
+      window.opener.postMessage(
+        "authorization:github:success:" + message,
+        "*"
+      );
+    }
+    
+    setTimeout(function() { window.close(); }, 1000);
+  })();
+</script>
+<p>Autenticación completada. Puedes cerrar esta ventana.</p>
+</body>
+</html>`;
+      
+      return new Response(html, { 
+        headers: { 'Content-Type': 'text/html' } 
+      });
     }
 
     return new Response('Not found', { status: 404 });
