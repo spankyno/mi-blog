b0bb4d2eb282b99c9c58c90484eb9081e9f3f575
Kalbo <spankyelmalo@yahoo.es>
2026-04-22 10:21:38 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { defineMiddleware } from 'astro:middleware';
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
 
-  if (!url.pathname.startsWith('/admin')) {
+  if (!url.pathname.startsWith('/panel')) {
     return next();
   }
 
